761ae1da152a962dd6371df3eadedcc217c605b8
Zoi-Gaiz <65063285+Zoi-Gaiz@users.noreply.github.com>
2021-03-22 08:40:19 +0800

    typedef a structure

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+typedef struct OLnode
+{
+    int i,j;
+    int e;
+    struct OLnode *down,*right;
+}OLNode,*OLink;
+
+typedef struct
+{
+    int mu,nu,tu;
+    OLink *rhead,*chead;
+}CrossList;
 
 
 
